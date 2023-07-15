7d1a1fa2bbd0de48898fdbe07f05928fe4975a76
anil.dhangar <anil.dhangar@qorbital.com>
2023-07-15 14:21:04 +0530

    fix

--- src/main/java/com/example/demo/controller/WelComeController.java
+++ src/main/java/com/example/demo/controller/WelComeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelComeController {
 
-    @GetMapping("/index")
+    @GetMapping("/hello")
     public String welcome(Model m){
         m.addAttribute("name","Anil");
         return "index";
